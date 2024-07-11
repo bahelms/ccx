ba4f8e22a0ca1404917c228608a082831b3da7c8
Jimbonk Buchanon <jimbonkgit@gmail.com>
2024-07-11 09:03:53 -0400

    Parse expression and statement

--- c++/parser.cpp
+++ c++/parser.cpp
@@ -1,43 +1,57 @@
 #include <iostream>
+#include <memory>
 
 #include "doctest.h"
 #include "lexer.h"
 #include "parser.h"
 
 void Parser::parse() {
-  for (auto token : _tokens) {
-    std::cout << "Token: " << token.value() << std::endl;
-  }
-  /* Program ast; */
-  /* return ast; */
+    for (auto token : _tokens) {
+        std::cout << "Token: " << token.value() << std::endl;
+    }
+    /* Program ast; */
+    /* return ast; */
 }
 
 void Parser::expect(std::string expected) {
-  Token &actual = _tokens[_current_token++];
-  if (actual.value() != expected) {
-    throw SyntaxError(
-        std::format("Expected {}, but got {}", expected, actual.value()));
-  }
+    const Token &actual = _tokens[_current_token++];
+    if (actual.value() != expected) {
+        throw SyntaxError(
+            std::format("Expected {}, but got {}", expected, actual.value()));
+    }
 }
 
-Exp Parser::parse_exp() {
-  Token &token = _tokens[_current_token++];
-  return Constant(token.value());
+std::unique_ptr<Statement> Parser::parse_statement() {
+    expect("return");
+    auto exp = parse_exp();
+    expect(";");
+    return std::make_unique<Return>(std::move(exp));
+}
+
+std::unique_ptr<Exp> Parser::parse_exp() {
+    const Token &token = _tokens[_current_token++];
+    return std::make_unique<Constant>(token.value());
+}
+
+TEST_CASE("Parser::parse_statement") {
+    std::vector<Token> tokens{{"return"}, {"1234"}, {";"}};
+    Parser parser(tokens);
+    auto stmt = parser.parse_statement();
+    CHECK(stmt->to_string() == "Return(\n  Constant(1234)\n)");
 }
 
 TEST_CASE("Parser::parse_exp") {
-  Parser parser(std::vector<Token>({{"100"}}));
-  Exp exp = parser.parse_exp();
-  CHECK(exp.value() == "100");
+    Parser parser(std::vector<Token>({{"100"}}));
+    auto exp = parser.parse_exp();
+    CHECK(exp->to_string() == "Constant(100)");
 }
 
 TEST_CASE("expressions have string representations") {
-  Constant exp{"42"};
-  CHECK(exp.to_string() == "Constant(42)");
+    Constant exp{"42"};
+    CHECK(exp.to_string() == "Constant(42)");
 }
 
 TEST_CASE("statements have string representations") {
-  Constant exp("23");
-  Return stmt(exp);
-  CHECK(stmt.to_string() == "Return(\n  Constant(23)\n)");
+    Return stmt(std::make_unique<Constant>("23"));
+    CHECK(stmt.to_string() == "Return(\n  Constant(23)\n)");
 }
--- c++/parser.h
+++ c++/parser.h
@@ -1,75 +1,80 @@
 #pragma once
 
 #include <format>
+#include <memory>
 
 #include "lexer.h"
 
 class Exp {
-public:
-  virtual std::string const to_string() = 0;
+  public:
+    virtual ~Exp() = default;
+
+    virtual std::string const to_string() = 0;
 };
 
 class Constant : public Exp {
-  std::string _value;
+    std::string _value;
 
-public:
-  Constant(std::string v) : _value(v) {}
+  public:
+    Constant(std::string v) : _value(v) {}
 
-  std::string value() const { return _value; }
-  std::string const to_string() override {
-    return std::format("Constant({})", _value);
-  }
+    std::string const to_string() override {
+        return std::format("Constant({})", _value);
+    }
 };
 
 class Statement {
-public:
-  virtual std::string const to_string() = 0;
+  public:
+    virtual ~Statement() = default;
+
+    virtual std::string const to_string() = 0;
 };
 
 class Return : public Statement {
-  Exp &_exp;
+    std::unique_ptr<Exp> _exp;
 
-public:
-  Return(Exp &e) : _exp(e) {}
+  public:
+    Return(auto e) : _exp(std::move(e)) {}
 
-  std::string const to_string() {
-    return std::format("Return(\n  {}\n)", _exp.to_string());
-  }
+    std::string const to_string() {
+        return std::format("Return(\n  {}\n)", _exp->to_string());
+    }
 };
 
 class Function {
-  std::string _name{};
-  Statement &_body;
+    std::string _name{};
+    Statement &_body;
 
-public:
-  Function(auto n, auto b) : _name(n), _body(b) {}
+  public:
+    Function(auto n, auto b) : _name(n), _body(b) {}
 
-  std::string to_string() {
-    return std::format("Function(\n  name={},\n  body={})", _name,
-                       _body.to_string());
-  }
+    std::string to_string() {
+        return std::format("Function(\n  name={},\n  body={})", _name,
+                           _body.to_string());
+    }
 };
 
 class Program {
-  Function _fn;
+    Function _fn;
 
-public:
-  Program(auto fn) : _fn(fn) {}
+  public:
+    Program(auto fn) : _fn(fn) {}
 
-  std::string to_string() {
-    return std::format("Program(\n  {}\n)", _fn.to_string());
-  }
+    std::string to_string() {
+        return std::format("Program(\n  {}\n)", _fn.to_string());
+    }
 };
 
 class Parser {
-  std::vector<Token> _tokens;
-  size_t _current_token{0};
+    std::vector<Token> _tokens;
+    size_t _current_token{0};
 
-  void expect(std::string);
+    void expect(std::string);
 
-public:
-  Parser(auto t) : _tokens(t) {}
+  public:
+    Parser(auto t) : _tokens(t) {}
 
-  void parse();
-  Exp parse_exp();
+    void parse();
+    std::unique_ptr<Exp> parse_exp();
+    std::unique_ptr<Statement> parse_statement();
 };
