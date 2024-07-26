6ce9be644d19587445070115e6131b7a8c260f69
Jimbonk Buchanon <jimbonkgit@gmail.com>
2024-07-26 07:41:58 -0400

    Finish lexical stage for Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,25 @@
+mod lexer;
+
+use lexer::Lexer;
+use std::{fs::File, io::BufReader};
+
+const LEX: i32 = 0;
+
 fn main() {
-    let filename = std::env::args().nth(1).expect("No filename given");
-    println!("Compiling file: {}", filename);
+    let filename = std::env::args().nth(1).expect("Error: No filename given");
+    let stage: i32 = std::env::args()
+        .nth(2)
+        .expect("Error: No stage given")
+        .parse()
+        .expect("Error: Stage must be an integer");
+    let file = File::open(filename).expect("File not found");
+    let reader = BufReader::new(file);
+
+    let mut lexer = Lexer::new(reader);
+    let tokens = lexer.generate_tokens();
+    if stage == LEX {
+        for token in tokens {
+            println!("{:?}", token);
+        }
+    }
 }
