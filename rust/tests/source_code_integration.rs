09f0dc0a0e14f3d6c84dee79d5e2f2406bf1fa1b
Barrett Helms <barrett.helms@salesloft.com>
2024-07-31 07:29:49 -0400

    Add integration test

---
+++ rust/tests/source_code_integration.rs
@@ -0,0 +1,20 @@
+use std::io::{BufReader, Cursor};
+
+#[test]
+fn compile_simple_program() {
+    let input = r#"
+    int main(void) {
+        return 101;
+    }
+    "#;
+    let reader = BufReader::new(Cursor::new(input.to_string()));
+    ccx::compile(reader, "integration_test_output.c".to_string(), 3);
+
+    let expected_output = "\t.globl main\nmain:\n\tmovl $101, %eax\n\tret\n\t.section .note.GNU-stack,\"\",@progbits\n";
+    let contents = std::fs::read_to_string("integration_test_output.s").expect("File not found");
+    assert_eq!(contents, expected_output);
+
+
+    std::fs::remove_file("integration_test_output.s");
+
+}
